adcebbc42c10dc376e9856e8cd635fac6ebee923
hwy15972 <Huangweiyue1314>
2020-12-14 23:37:46 +0800

    chapter9 item60 float double精度丢失

--- src/effectivejava/chapter9/item60/FloatOrDoubleWrongExample.java
+++ src/effectivejava/chapter9/item60/FloatOrDoubleWrongExample.java
@@ -3,6 +3,7 @@ package effectivejava.chapter9.item60;
 /**
  * huangweiyue
  * float和double会造成精度缺失
+ * 不声明的时候，默认小数都用double来表示，所以如果要用float的话，则应该在其后加上f 
  */
 public class FloatOrDoubleWrongExample {
     public static void main(String[] args) {
