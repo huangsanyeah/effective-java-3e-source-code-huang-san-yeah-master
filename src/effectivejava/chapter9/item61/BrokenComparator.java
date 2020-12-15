2aa9a505e617d4dc4a58a91581de2febc34b839f
huangweiyue <huangweiyue@quyiyuan.com>
2020-12-15 16:22:36 +0800

    item61 基本类型优先于包装类型

--- src/effectivejava/chapter9/item61/BrokenComparator.java
+++ src/effectivejava/chapter9/item61/BrokenComparator.java
@@ -1,16 +1,31 @@
 package effectivejava.chapter9.item61;
-import java.util.*;
 
-// Broken comparator - can you spot the flaw? - Page 273
+import java.util.Comparator;
+
+
+/**
+ * 尽量用基本类型 不要使用包装类型
+ */
 public class BrokenComparator {
     public static void main(String[] args) {
 
+        /**
+         * 示例1：比较器将错误地返回 1
+         * 计算表达式 i < j 会使 i 和 j 引用的 Integer 实例自动拆箱；
+         * 也就是说，它提取它们的基本类型值。计算的目的是检查得到的第一个 int 值是否小于第二个 int 值。
+         * 但假设它不是。然后，下一个测试计算表达式 i==j，该表达式对两个对象引用执行标识比较。
+         * 如果 i 和 j 引用表示相同 int 值的不同 Integer 实例，这个比较将返回 false，比较器将错误地返回 1，表明第一个整型值大于第二个整型值。
+         * 将 == 操作符应用于包装类型几乎都是错误的
+         * 修复办法：i == j 改为equals()
+         */
 //        Comparator<Integer> naturalOrder =
 //                (i, j) -> (i < j) ? -1 : (i == j ? 0 : 1);
 
-       // Fixed Comparator - Page 274
+        // Fixed Comparator - Page 274
         Comparator<Integer> naturalOrder = (iBoxed, jBoxed) -> {
-            int i = iBoxed, j = jBoxed; // Auto-unboxing
+            int i = iBoxed, j = jBoxed;
+            // Auto-unboxing
+            //return Integer.compare(i, j);
             return i < j ? -1 : (i == j ? 0 : 1);
         };
 
--- src/effectivejava/chapter9/item61/Unbelievable.java
+++ src/effectivejava/chapter9/item61/Unbelievable.java
@@ -1,11 +1,32 @@
 package effectivejava.chapter9.item61;
 
 // What does this program do? - Page 274
+
+/**
+ * 程序员忽略了基本类型和包装类型之间的区别，并承担了恶果。在前两个项目中，结果是彻底的失败；第三个例子还产生了严重的性能问题。
+ */
 public class Unbelievable {
     static Integer i;
 
     public static void main(String[] args) {
-        if (i == 42)
+        //示例2：程序员忽略了基本类型和包装类型之间的区别，不可预期的报错 NPE
+       /* if (i == 42) {
             System.out.println("Unbelievable");
+        }*/
+
+
+        //示例3：用包装类型 性能极差
+        long begin=System.currentTimeMillis();
+//        Long sum = 0L;
+        long sum = 0;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        long end=System.currentTimeMillis();
+        System.out.println(sum);
+        // Long sum = 0L; 性能差 8174
+        System.out.println(end-begin);
+        //long sum = 0;性能好 1419
+        System.out.println(end-begin);
     }
 }
